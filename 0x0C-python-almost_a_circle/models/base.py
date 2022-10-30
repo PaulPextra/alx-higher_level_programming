145a0de34515bd7fc3357e6db009514cdcd8a31f
Paul Okoli <paulokoli7@gmail.com>
2022-10-30 13:54:21 +0100

    Adding models/base.py, models/__init__.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 """Defines a base model class."""
-import json
-import csv
-import turtle
-
 class Base:
     """Represent the base model.
     Represents the "base" for all other classes in project 0x0C*.
