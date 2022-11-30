3d7a31adfff5e56fbd60d6622dd5ff754b5a457e
Paul Okoli <paulokoli7@gmail.com>
2022-11-30 20:17:58 +0100

    Add 2-create_read_user.sql file

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,12 +1,7 @@
--- Creates the database hbtn_0d_2 and the user user_0d_2
--- The user_0d_2 has SELECT privilege on hbtn_0d_2 with password user_0d_2_pwd
-CREATE DATABASE
-    IF NOT EXISTS `hbtn_0d_2`;
-CREATE USER
-    IF NOT EXISTS 'user_0d_2'@'localhost'
-    IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT
-   ON `hbtn_0d_2`.*
-   TO 'user_0d_2'@'localhost'
-   IDENTIFIED BY 'user_0d_2_pwd';
-FLUSH PRIVILEGES;
+-- Create a database and user
+-- DDL query to create an user with specific privileges
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+-- query privilegs for user_0d_2
+GRANT USAGE ON * . * TO 'user_0d_2'@'localhost';
+GRANT SELECT ON `hbtn_0d_2` . * TO 'user_0d_2'@'localhost';
